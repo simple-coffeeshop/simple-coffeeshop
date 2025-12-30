40a4ec5d495e33a291cbe6756f055fa3b827f810
Chuvashev Maxim <plus79634820353@gmail.com>
2025-12-31 01:12:43 +0400

    feat(ui): экспортировать новые компоненты из библиотеки

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,7 @@
 // packages/ui/src/index.ts
 export const UI_VERSION = "1.0.0";
-export * from "./components";
+export * from "./atoms/Badge/Badge";
+export * from "./atoms/Button/Button";
+export * from "./atoms/GlassCard/GlassCard";
+export * from "./atoms/Input/Input";
+export * from "./molecules/Table/Table";
